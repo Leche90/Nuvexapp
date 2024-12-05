56e75dec40a60a64355cf7abf6d1d9452b0aaac3
Lanzema Leche <lleche@DESKTOP-QQ49ALV>
2024-12-04 20:56:23 -0600

    updated migration files

--- db/migrate/20241113010841_add_province_id_to_addresses.rb
+++ db/migrate/20241113010841_add_province_id_to_addresses.rb
@@ -1,5 +1,7 @@
 class AddProvinceIdToAddresses < ActiveRecord::Migration[7.2]
   def change
-    add_reference :addresses, :province, null: false, foreign_key: true
+    unless column_exists?(:addresses, :province_id)
+      add_reference :addresses, :province, null: false, foreign_key: true
+    end
   end
 end
